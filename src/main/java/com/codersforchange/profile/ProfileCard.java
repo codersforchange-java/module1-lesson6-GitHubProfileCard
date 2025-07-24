de5011eb363afab61c0f3faa713959b0648b6233
Pascal Maximilian Haas <pascal.haas96@gmail.com>
2025-07-24 23:16:26 +0200

    feat: Add GitHub username to profile card

--- src/main/java/com/codersforchange/profile/ProfileCard.java
+++ src/main/java/com/codersforchange/profile/ProfileCard.java
@@ -2,6 +2,6 @@ package com.codersforchange.profile;
 
 public class ProfileCard {
     public static void main(String[] args) {
-        System.out.println("This GitHub profile belongs to: [YOUR GITHUB USERNAME HERE]");
+        System.out.println("This GitHub profile belongs to: pmh96");
     }
 }
